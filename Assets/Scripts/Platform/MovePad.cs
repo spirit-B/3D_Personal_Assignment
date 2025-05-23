e7816e10953ceef9f0cda7d6f4720c07e85d29de
JangHoon Ban <janghoon333@naver.com>
2025-05-23 12:36:14 +0900

    feat : 위아래로 움직이는 발판 제작

---
+++ Assets/Scripts/Platform/MovePad.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePad : MonoBehaviour
+{
+	public float moveSpeed;
+	public float moveDistance;
+	private float waitTime = 3f;
+
+	private Vector3 startPosition;
+	private Vector3 topPosition;
+	private Vector3 bottomPosition;
+	private bool movingUp = true;
+	private bool isWaiting = false;
+	private float waitTimer = 0f;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		startPosition = transform.position;
+		topPosition = startPosition + Vector3.up * moveDistance;
+		bottomPosition = startPosition;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (isWaiting)
+		{
+			waitTimer += Time.deltaTime;
+			if (waitTimer >= waitTime)
+			{
+				isWaiting = false;
+				waitTimer = 0f;
+				movingUp = !movingUp;
+			}
+			return;
+		}
+
+		Vector3 targetPosition = movingUp ? topPosition : bottomPosition;
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+		if (Vector3.Distance(transform.position, targetPosition) <= 0.01f)
+		{
+			Debug.Log("목적지 도착");
+			isWaiting = true;
+			Debug.Log($"isWaiting : {isWaiting}");
+		}
+	}
+}
